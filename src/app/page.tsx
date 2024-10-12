e563c79413e6e4414947ce88d845304d30b95ab5
krishna10k <krishnaprasadawala@gmail.com>
2024-10-12 23:05:26 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 
 export default function Home() {
 
-
+  const [clicked, setClicked] = useState("")
+  const [isOpen, setIsOpen] = useState<string[]>([])
 
   const shortcuts = [
     {
@@ -30,22 +31,34 @@ export default function Home() {
     }
   ];
 
+  const closeWindow = (windowName : string) => {
+    setIsOpen(isOpen.filter(openWindow => openWindow !== windowName))
+  }
   
   return (
     <>
     <div className="flex flex-col gap-7 mt-5">
       {
-        shortcuts.map((shortcut, i) => {
+        shortcuts.map((shortcut) => {
           return (
-            <div  className=" w-20 h-auto flex flex-col gap-2 text-center text-wrap justify-center items-center hover:bg-blue-200/20 " key={shortcut.id}>
+            <div onClick={() => setClicked(shortcut.name)}
+            onDoubleClick={()=> setIsOpen([...isOpen, shortcut.name])}
+             className=" w-20 h-auto flex flex-col gap-2 text-center text-wrap justify-center items-center hover:bg-blue-200/20 " 
+             key={shortcut.id}
+             style={{background : clicked == shortcut.name ? "#2E2D49" : ""
+
+             }}
+             >
               <img className="w-10 select-none" src={shortcut.image} alt="Icon" />
-              <p className="select-none text-sm select-none">{shortcut.name}</p>
+              <p className="select-none text-sm">{shortcut.name}</p>
             </div>
           )
         })
       }
     </div>
-    <UserFolder  />
+    {
+      isOpen.includes("Krishna") && <UserFolder closeWindow = {closeWindow} />
+    }
     </>
   );
 }
--- src/components/UserFolder.tsx
+++ src/components/UserFolder.tsx
@@ -5,9 +5,10 @@ import { FaRegWindowMaximize } from "react-icons/fa6";
 import { IoCloseSharp } from "react-icons/io5";
 import { FaMinus } from "react-icons/fa6";
 
-const UserFolder = () => {
+const UserFolder = ({closeWindow} : {closeWindow : Function}) => {
   const [posX, setPosX] = useState(1000);
   const [posY, setPosY] = useState(200);
+  const [isMaximized, setIsMaximized] = useState(false);
 
 
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
@@ -46,17 +47,20 @@ const UserFolder = () => {
   return (
     <div
       style={{
-        top: `${posY - 10}px`,
-        left: `${posX - 550}px`,
-        position: "fixed", 
+        width : isMaximized ? "100vw" : "60vw",
+        height : isMaximized ? "100vh" : "60vh",
+        top: isMaximized ? 0 :  `${posY - 10}px`,
+        left: isMaximized ? 0 : `${posX - 550}px`,
+        position: "fixed",
+        transition: isMaximized ? "top 0.3s ease, left 0.3s ease, width 0.3s ease, height 0.3s ease" : "none",
       }}
-      className="w-[60vw] h-[60vh] bg-gray-900 rounded-md"
+      className="bg-gray-900 rounded-md"
       onDragOver={handleDragOver}
     >
         <div draggable onDragStart={handleDragStart}  onDrag={(e) => moveFolder(e)} onDragEnd={(e) => finalizePosition(e)}   className="w-full h-10 bg-gray-800/50 backdrop-blur-sm relative top-0 rounded-t-md flex justify-end">
             <div className="w-10 hover:bg-gray-500 h-full flex justify-center items-center"><FaMinus size={10} /></div>
-            <div className="w-10 hover:bg-gray-500 h-full flex justify-center items-center">< FaRegWindowMaximize size={10}/></div>
-            <div className="w-10 hover:bg-red-500 h-full flex justify-center items-center"><IoCloseSharp /></div>
+            <div className="w-10 hover:bg-gray-500 h-full flex justify-center items-center" onClick={() => setIsMaximized(!isMaximized)}>< FaRegWindowMaximize size={10}/></div>
+            <div className="w-10 hover:bg-red-500 h-full flex justify-center items-center" onClick={() => closeWindow("Krishna")} ><IoCloseSharp /></div>
         </div>
         <h1>Hi! I am Krishna</h1>
     </div>
