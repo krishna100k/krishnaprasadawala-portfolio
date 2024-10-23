b277373fca95aa9aa68a1fd95beabea0e3dea425
krishna10k <krishnaprasadawala@gmail.com>
2024-10-24 00:48:05 +0530

    changes

--- src/components/MiniProjectCard.tsx
+++ src/components/MiniProjectCard.tsx
@@ -3,8 +3,8 @@ import { MiniProject } from "@/lib/data"
 
 const MiniProjectCard : React.FC<{projectData : MiniProject}> = ({projectData}) => {
   return (
-    <div onClick={() => window.open(projectData.liveLink, "_blank")} className="md:w-[98%] w-full h-14 flex items-center pl-5 bg-[url('/background-2.jpg')] bg-cover border border-gray-800 cursor-pointer">
-        <p ><span className="font-bold"> {projectData.projectName} </span> - {projectData.projectDescription}</p>
+    <div onClick={() => window.open(projectData.liveLink, "_blank")} className="md:w-[98%] w-full min-h-14 flex items-center pl-5 bg-[url('/background-2.jpg')] bg-cover border border-gray-800 cursor-pointer">
+        <p className="py-4" ><span className="font-bold "> {projectData.projectName} </span> - {projectData.projectDescription}</p>
     </div>
   )
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -16,29 +16,37 @@ const ProjectCard: React.FC<{ projectData: MajorProject }> = ({projectData}) => 
   }
 
   return (
-    <div className={` w-full md:w-[48%] h-[25rem] bg-cover bg-no-repeat border border-gray-800 relative overflow-hidden`} style={{ backgroundImage: `url(${projectData.image})` }}>
-      <div onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler} className="w-full h-14 hover:h-full  bg-black/55 backdrop-blur-lg absolute bottom-0 transition-all duration-500 border-t border-gray-800 cursor-pointer">
+    <div className={` w-full md:w-[48%] min-h-[25rem]  bg-cover bg-no-repeat border border-gray-800 relative overflow-hidden`} style={{ backgroundImage: `url(${projectData.image})` }}>
+      <div onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler} className="w-full h-14 hover:h-full overflow-y-auto overflow-x-hidden bg-black/55 backdrop-blur-lg absolute bottom-0 transition-all duration-500 border-t border-gray-800 cursor-pointer">
         <div className="flex items-center">
           <p className="mt-4 ml-5 mr-2 font-bold ">{projectData.projectName}</p>
           {isHovering ? <FaChevronDown className="mt-4 mr-5 text-sm " /> : <FaChevronUp className="mt-4 mr-5 text-sm " />}
         </div>
-        <ul className="mt-5 ml-8 mr-5 list-disc text-sm flex flex-col gap-1">
-          {projectData.projectDescription.map((list) => (
-            <li>{list}</li>
-          ))}
-        </ul>
-        <div className="flex w-full gap-3 mt-5 ml-5">
-          <a href={projectData.github} target="_blank">
-            <button className="bg-[#ededed] text-black p-2 rounded-sm hover:bg-[#121212] hover:text-[#ededed] transition-all ease-in-out duration-300 ">
-              Github
-            </button>
-          </a>
-          <a href={projectData.liveLink} target="_blank">
-            <button className="bg-[#121212] text-[#ededed] p-2 rounded-sm hover:text-[#09090B] hover:bg-[#ededed] border transition-all ease-in-out duration-300">
-              Live
-            </button>
-          </a>
-        </div>
+
+        {
+          isHovering && (
+            <>
+            <ul className="mt-5 ml-8 mr-5 list-disc text-sm flex flex-col gap-1">
+            {projectData.projectDescription.map((list) => (
+              <li>{list}</li>
+            ))}
+          </ul>
+          <div className="flex w-full gap-3 mt-5 ml-5">
+            <a href={projectData.github} target="_blank">
+              <button className="bg-[#ededed] text-black p-2 rounded-sm hover:bg-[#121212] hover:text-[#ededed] transition-all ease-in-out duration-300 ">
+                Github
+              </button>
+            </a>
+            <a href={projectData.liveLink} target="_blank">
+              <button className="bg-[#121212] text-[#ededed] p-2 rounded-sm hover:text-[#09090B] hover:bg-[#ededed] border transition-all ease-in-out duration-300">
+                Live
+              </button>
+            </a>
+          </div>
+          </>
+          )
+        }
+
       </div>
     </div>
   );
